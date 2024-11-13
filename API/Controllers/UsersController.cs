645c451b2ed74590a9d4ed94b478ee87728cc675
AhmadIssa02 <issa.ahmad2002@gmail.com>
2024-11-13 11:37:14 +0300

    remove testing lines

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -18,25 +18,21 @@ namespace API.Controllers
         {
             var users = await context.Users.ToListAsync();
 
-            Console.WriteLine("hello");
-
             // Loop through the users and print their details
             foreach (var user in users)
             {
                 Console.WriteLine($"User ID: {user.Id}, Username: {user.Username}");
             }
-            Console.WriteLine("bye");
-
 
             return users;
         }
 
         [HttpGet("{id:int}")]
-        public async Task <ActionResult<AppUser>> GetUsers(int id)
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
         {
             var user = await context.Users.FindAsync(id);
 
-            if (user == null){return NotFound();}
+            if (user == null) { return NotFound(); }
 
             return user;
         }
