ac33696d1692cab29e7e99ecec617bc4e17a7d14
AhmadIssa02 <issa.ahmad2002@gmail.com>
2024-12-01 10:05:21 +0300

    Initial Commit

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController(DataContext context) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if(await UserExists(registerDto.username)) return BadRequest("username already exists");
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                Username = registerDto.username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.password)),
+                PasswordSalt = hmac.Key
+            };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return Ok(user);
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.fir
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await context.Users.AnyAsync(x=> x.Username.ToLower() == username.ToLower());
+        }
+    }
+    
+}
